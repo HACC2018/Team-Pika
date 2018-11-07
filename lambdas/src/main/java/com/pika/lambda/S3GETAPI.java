9ebd9013262b40898dc7bacc0875a853a3e6a9bd
lihongman <herman_hmli@yahoo.com>
2018-11-06 17:41:08 -1000

    Renamed file and added Apache Spark

--- lambdas/src/main/java/com/pika/lambda/S3GETAPI.java
+++ lambdas/src/main/java/com/pika/lambda/S3GETAPI.java
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class DatabaseToS3 {
+public class S3GETAPI {
 
     public static class RequestClass {
         public RequestClass(String starttime, String endtime) {
