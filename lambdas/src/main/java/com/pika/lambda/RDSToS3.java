616e58c7cdf531c22f8bfe8852395fc46725373e
Sean Takafuji <seanytak@hawaii.edu>
2018-11-06 22:03:41 -1000

    Added S3 Dump

--- lambdas/src/main/java/com/pika/lambda/RDSToS3.java
+++ lambdas/src/main/java/com/pika/lambda/RDSToS3.java
@@ -2,7 +2,11 @@ package com.pika.lambda;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.Calendar;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.apache.commons.lang3.StringUtils;
 
@@ -24,20 +28,30 @@ public class RDSToS3 {
             String sql = "SELECT * FROM hacc.energy";
             ResultSet rs = stmt.executeQuery(sql);
 
+            // Intermediate Java Object of the Data
             ResultSetMetaData rsMetaData = rs.getMetaData();
             int columnCount = rsMetaData.getColumnCount();
             JsonObject obj = new JsonObject();
 
+            // Amazon S3
+            Calendar cal = Calendar.getInstance();
+            AmazonS3 s3 = AmazonS3ClientBuilder.defaultClient();
+            String bucketName = "teampika-tempbucket";
+
             while (rs.next()) {
-                obj.addProperty(rsMetaData.getColumnName(1), rs.getString(1));
+                Timestamp timestamp = rs.getTimestamp(1);
+                obj.addProperty(rsMetaData.getColumnName(1), timestamp.toString());
                 for (int i = 2; i <= columnCount; i++) {
                     String columnName = rsMetaData.getColumnName(i);
                     String prettyColumnName = StringUtils.capitalize(columnName.replace("_", " "));
                     obj.addProperty(prettyColumnName, rs.getDouble(columnName));
                 }
-            }
 
-            System.out.println(obj);
+                cal.setTimeInMillis(timestamp.getTime());
+                String filename = String.format("%d/%d/%d/%d.json", cal.get(Calendar.YEAR), cal.get(Calendar.MONTH),
+                        cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.HOUR_OF_DAY));
+                s3.putObject(bucketName, filename, obj.toString());
+            }
 
             stmt.close();
             conn.close();
